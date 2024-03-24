cede5c0f6fe4778e7ae966a9de6b67339e879d94
Ildar Sabirov <ildar-sabirov-20-06@mail.ru>
2024-03-24 03:19:58 +0300

    Update pdb-to-pdbqt-removing-water.sh

--- pdb-to-pdbqt-removing-water.sh
+++ pdb-to-pdbqt-removing-water.sh
@@ -3,8 +3,8 @@ dir="pdb-to-pdbqt"
 
 for file in "$dir"/*
 do
-  grep -v 'HOH' $file > $file1
-  file1="$file"
-  file2="${file%.*}.pdbqt"
+  file1="${file%.*}_noHOH.pdb" # output file will have _noHOH.txt suffix
+  grep -v 'HOH' "$file" > "$file1"
+  file2="${file%.*}_removed_water.pdbqt"
   obabel "$file1" -O "$file2"
 done
